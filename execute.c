229fae9133ed11c49636c3fae627b55f131db3b1
Giraluna1 <lunagolo@hotmail.com>
2020-11-25 19:43:05 -0500

    Leak memory in empty line fix

--- execute.c
+++ execute.c
@@ -4,7 +4,7 @@
  * execute - executes a program
  *
  * @command: The command to be executed (includes path)
- * @args: The arguments of the program
+ * @args: The arguments of the program to bbe execute
  */
 
 void execute(char *command, char **args)
