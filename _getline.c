211221687e965f178d8b16c67d392b55b79473e3
Lucho Patiño <2450@holbertonschool.com>
2020-12-03 13:26:08 +0000

    Jarger buffer in NIM

--- _getline.c
+++ _getline.c
@@ -55,7 +55,7 @@ char **_getline(void)
 
 char **_getline_NIM(void)
 {
-	char buf[4096], **command_token = NULL, **command = NULL, *buffer;
+	char buf[8192], **command_token = NULL, **command = NULL, *buffer;
 	int i = 0, k = 0;
 	pid_t child_detect;
 
